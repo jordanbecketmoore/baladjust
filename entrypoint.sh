6c2f363e5696a6875ec3577349aa9713457c98c4
Jordan Moore <jordanbecketmoore@protonmail.com>
2026-02-23 09:38:09 -0500

    fix conditional logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ SETUP_TOKEN=$(jq -r '.simplefin.setupToken // empty' /app/config.json)
 cp /app/config.json /tmp/config.json.tmp
 
 # If neither simplefin.accessUrl nor simplefin.setupToken is configured, and both SIMPLEFIN_ACCESSURL and SIMPLEFIN_SETUPTOKEN environment variables are set, update config.json with the values from the environment variables
-if ([ -z "$ACCESS_URL" ] || [ "$ACCESS_URL" != "null" ]) && ([ -z "$SETUP_TOKEN" ] || [ "$SETUP_TOKEN" != "null" ] || [ "$SETUP_TOKEN" != "PASTE_SETUP_TOKEN_HERE" ]); then
+if ([ -z "$ACCESS_URL" ] || [ "$ACCESS_URL" == "null" ]) && ([ -z "$SETUP_TOKEN" ] || [ "$SETUP_TOKEN" == "null" ] || [ "$SETUP_TOKEN" == "PASTE_SETUP_TOKEN_HERE" ]); then
 # Check for access url in env
   if [ -n "$SIMPLEFIN_ACCESSURL" ]; then
     echo "Setting simplefin.accessUrl from SIMPLEFIN_ACCESSURL environment variable"
